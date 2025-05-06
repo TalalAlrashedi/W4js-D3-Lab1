79420c571ca186a84d30f57be5e93d6b1ce9a94d
TalalAlrashedi <TalalAlrashedi24@hotmail.com>
2025-05-06 11:50:16 +0300

    lab 3 done

--- lab3.js
+++ lab3.js
@@ -32,16 +32,6 @@ const movies = [
   { title: "Fight Club", director: "David Fincher", year: 1999, rating: 8.8 },
 ];
 
-// 4. احصل على الأفلام التي تم إصدارها قبل عام 2010.
-// 5. احصل على مصفوفة تحتوي فقط عناوين الأفلام.
-// 6. احصل على متوسط تقييم جميع الأفلام.
-// 7. قم بترتيب الأفلام حسب السنة (الأقدم إلى الأحدث).
-// 8. قم بترتيب الأفلام حسب التقييم (من الأعلى إلى الأقل).
-// 9. هل جميع الأفلام تقييمها أعلى من 8؟
-// 10. هل يوجد فيلم واحد على الأقل تم إصداره في عام 1999؟
-// 11. هل جميع الأفلام أخرجها نفس المخرج؟
-// 12. هل يوجد فيلم واحد على الأقل تقييمه أقل من 8.5؟
-
 // 1. احصل على عنوان أول فيلم تم إصداره بعد عام 2000.
 
 let firstMovie = movies.find((movie) => movie.year > 2000);
@@ -65,3 +55,73 @@ let nolanMoviesTitle = nolanMovies.map((title) => title.title);
 console.log("--------------------------");
 
 console.log(nolanMoviesTitle);
+
+console.log("--------------------------");
+
+// 4. احصل على الأفلام التي تم إصدارها قبل عام 2010.
+
+let moviesBefore2010 = movies
+  .filter((movie) => movie.year < 2010)
+  .map((title) => title.title);
+
+console.log(moviesBefore2010);
+console.log("--------------------------");
+
+// 5. احصل على مصفوفة تحتوي فقط عناوين الأفلام.
+
+let onlyTitleMovies = movies.map((title) => title.title);
+
+console.log(onlyTitleMovies);
+console.log("--------------------------");
+
+// 6. احصل على متوسط تقييم جميع الأفلام.
+
+let totalRates = movies.reduce((sum, rate) => sum + rate.rating, 0);
+
+let avgRates = totalRates / movies.length;
+
+console.log(avgRates);
+console.log("--------------------------");
+
+// 7. قم بترتيب الأفلام حسب السنة (الأقدم إلى الأحدث).
+
+let sortYearMovies = movies.sort((a, b) => a.year - b.year);
+
+console.log(sortYearMovies);
+console.log("--------------------------");
+
+// 8. قم بترتيب الأفلام حسب التقييم (من الأعلى إلى الأقل).
+
+let sortRates = movies.sort((a, b) => b.rating - a.rating);
+
+console.log(sortRates);
+console.log("--------------------------");
+// 9. هل جميع الأفلام تقييمها أعلى من 8؟
+
+let everyMovieHigher8 = movies.every((movie) => movie.rating);
+
+console.log(everyMovieHigher8);
+console.log("--------------------------");
+
+// 10. هل يوجد فيلم واحد على الأقل تم إصداره في عام 1999؟
+
+let movieRealse1999 = movies.some((movie) => movie.year == 1999);
+
+console.log(movieRealse1999);
+
+console.log("--------------------------");
+
+// 11. هل جميع الأفلام أخرجها نفس المخرج؟
+
+let sameDirector = movies[0].director;
+
+let allDirector = movies.every((movie) => movie.director == sameDirector);
+
+console.log(allDirector);
+console.log("--------------------------");
+
+// 12. هل يوجد فيلم واحد على الأقل تقييمه أقل من 8.5؟
+
+let lessThan8 = movies.some((movie) => movie.rating < 8.5);
+
+console.log(lessThan8);
